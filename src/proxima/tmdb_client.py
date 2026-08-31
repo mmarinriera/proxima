d63406d3926e567e5580b9a033d2516e524b2abf
Miquel Marin Riera <miquel.marin.riera@gmail.com>
2026-08-31 10:31:56 +0200

    refactor(tmdb): reduce cache ttl some more

--- src/proxima/tmdb_client.py
+++ src/proxima/tmdb_client.py
@@ -10,7 +10,7 @@ from hishel.httpx import SyncCacheClient
 logger = logging.getLogger(__name__)
 
 MAX_N_RESULTS = 100
-DEFAULT_CACHE_TTL = 3600 * 2  # 2h
+DEFAULT_CACHE_TTL = 60 * 30  # 30min
 DEFAULT_CACHE_PATH = ".cache/hishel/hishel_cache.db"
 
 policy = SpecificationPolicy(
