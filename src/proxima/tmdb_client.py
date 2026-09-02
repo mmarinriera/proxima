9ee490c8ab58e4bab399ef53fef98e93c57e2c2b
Miquel Marin Riera <miquel.marin.riera@gmail.com>
2026-09-02 15:01:23 +0200

    refactor(tmdb): separate code that is async agnostic

--- src/proxima/tmdb_client.py
+++ src/proxima/tmdb_client.py
@@ -1,9 +1,12 @@
 import logging
+from dataclasses import dataclass
+from dataclasses import field
 from enum import Enum
 from typing import Annotated
 from typing import Any
 from typing import Self
 
+import httpx
 from hishel import CacheOptions
 from hishel import SpecificationPolicy
 from hishel import SyncSqliteStorage
@@ -21,6 +24,10 @@ LONG_TTL = 3600 * 24
 
 TIME_FIELD_MOVIE = "primary_release_date"
 TIME_FIELD_TV = "first_air_date"
+
+GENRE_QUERY_PARAMS = {"language": "en-US"}
+
+DEFAULT_CACHE_POLICY = SpecificationPolicy(cache_options=CacheOptions(shared=False))
 
 
 class GenreError(Exception):
@@ -83,18 +90,131 @@ class TVItem(TMDBItem):
     genres: Annotated[list[str], Field(default_factory=list)]
 
 
+class GenresManager:
+    def __init__(self, api_token: str) -> None:
+        self.api_token = api_token
+        self.genres = {
+            MediaCategory.movie: self._query_genres(MediaCategory.movie),
+            MediaCategory.tv: self._query_genres(MediaCategory.tv),
+        }
+
+        self.genres_encoder = {
+            MediaCategory.movie: {g["name"].lower(): g["id"] for g in self.genres[MediaCategory.movie]},
+            MediaCategory.tv: {g["name"].lower(): g["id"] for g in self.genres[MediaCategory.tv]},
+        }
+
+        self.genres_decoder = {
+            MediaCategory.movie: {g["id"]: g["name"] for g in self.genres[MediaCategory.movie]},
+            MediaCategory.tv: {g["id"]: g["name"] for g in self.genres[MediaCategory.tv]},
+        }
+
+    def _query_genres(self, category: MediaCategory) -> list[dict[str, Any]]:
+        headers = {
+            "Authorization": f"Bearer {self.api_token}",
+            "accept": "application/json",
+        }
+        response = httpx.get(url=f"{BASE_URL}/genre/{category.value}/list", params=GENRE_QUERY_PARAMS, headers=headers)
+        response.raise_for_status()
+        data: dict[str, list[dict[str, Any]]] = response.json()
+
+        return data["genres"]
+
+    def get_genres(self, category: MediaCategory) -> list[str]:
+        """Get list of TMDB coded genres from a media category."""
+        return [item["name"] for item in self.genres[category]]
+
+    def encode_genres(self, category: MediaCategory, input_genres: list[str]) -> list[int]:
+        genres_encoder: dict[str, int] = self.genres_encoder[category]
+        try:
+            encoded = [genres_encoder[name.lower()] for name in input_genres]
+        except KeyError as e:
+            raise GenreError(f"Invalid input genre: {e}.")
+        return encoded
+
+    def decode_genres(self, category: MediaCategory, results: list[dict[str, Any]]) -> list[dict[str, Any]]:
+        genres_decoder: dict[int, str] = self.genres_decoder[category]
+        try:
+            for item in results:
+                item["genres"] = [genres_decoder[gid] for gid in item["genre_ids"]]
+        except KeyError as e:
+            raise GenreError(f"Unknown genre while processing results: {e}.")
+        return results
+
+
+@dataclass
+class QueryParams:
+    category: MediaCategory
+    genres_manager: GenresManager
+    n_results: int
+    genres: list[str] | None = None
+    year_from: int | None = None
+    year_to: int | None = None
+    min_rating: float | None = None
+    min_votes: int | None = None
+    sort_by: str = "vote_average.desc"
+    language: str = "en-US"
+    include_adult: bool = False
+    include_video: bool = False
+    _n_results: int = field(init=False, repr=False)
+
+    @property
+    def n_results(self) -> int:
+        if self._n_results > MAX_N_RESULTS:
+            logger.warning(
+                f"Max number of results queried at once is {MAX_N_RESULTS}. Capped query to {MAX_N_RESULTS}.",
+            )
+            return MAX_N_RESULTS
+        return self._n_results
+
+    @n_results.setter
+    def n_results(self, val: int) -> None:
+        self._n_results = val
+
+    def parse(self) -> dict[str, Any]:
+        params: dict[str, Any] = {
+            "language": self.language,
+            "include_adult": self.include_adult,
+            "include_video": self.include_video,
+            "sort_by": self.sort_by,
+        }
+
+        if self.genres is not None:
+            params["with_genres"] = "|".join(
+                [str(g) for g in self.genres_manager.encode_genres(category=self.category, input_genres=self.genres)]
+            )
+            logger.debug(f"genres param {params['with_genres']}")
+
+        time_field = TIME_FIELD_MOVIE if self.category == MediaCategory.movie else TIME_FIELD_TV
+
+        if self.year_from is not None:
+            params[f"{time_field}.gte"] = f"{self.year_from}-01-01"
+
+        if self.year_to is not None:
+            params[f"{time_field}.lte"] = f"{self.year_to}-12-31"
+
+        if self.min_rating is not None:
+            params["vote_average.gte"] = self.min_rating
+
+        if self.min_votes is not None:
+            params["vote_count.gte"] = self.min_votes
+
+        return params
+
+
 class TMDBClient:
     def __init__(self, tmdb_api_token: str):
         self.api_token = tmdb_api_token
 
         self.client = SyncCacheClient(
             storage=SyncSqliteStorage(database_path=DEFAULT_CACHE_PATH, default_ttl=DEFAULT_CACHE_TTL),
-            policy=SpecificationPolicy(cache_options=CacheOptions(shared=False)),
+            policy=DEFAULT_CACHE_POLICY,
             headers={
                 "Authorization": f"Bearer {self.api_token}",
                 "accept": "application/json",
             },
         )
+
+        self.genres_manager = GenresManager(tmdb_api_token)
 
     def __enter__(self) -> Self:
         return self
@@ -123,66 +243,29 @@ class TMDBClient:
         return response.json()
 
     def _aggregate_results(self, url: str, params: dict[str, Any], n_results: int) -> list[dict[str, Any]]:
-        if n_results > MAX_N_RESULTS:
-            logger.warning(
-                f"Max number of results queried at once is {MAX_N_RESULTS}. Capped query to {MAX_N_RESULTS}.",
-            )
-            n_results = MAX_N_RESULTS
-
         results: list[dict[str, Any]] = []
         page = 0
         while len(results) < n_results:
             page += 1
             params["page"] = page
             logger.debug(f"querying page {page}")
+
             data = self._get(url, params)
+            results.extend(data["results"])
 
             if page == 1 and data["total_results"] < n_results:
                 logger.warning(f"Only {data['total_results']} results are available.")
                 n_results = data["total_results"]
 
-            results.extend(data["results"])
             logger.debug(f"results so far {len(results)}")
             if data["total_pages"] == page:
                 break
 
         return results[:n_results]
 
-    def _query_genres(self, category: MediaCategory) -> list[dict[str, Any]]:
-        params: dict[str, Any] = {
-            "language": "en-US",
-        }
-        extensions: dict[str, Any] = {
-            "hishel_ttl": LONG_TTL,  # Set long ttl for a request that rarely changes
-        }
-        data: dict[str, list[dict[str, Any]]] = self._get(
-            endpoint=f"/genre/{category.value}/list", params=params, extensions=extensions
-        )
-
-        return data["genres"]
-
-    def _encode_genres(self, category: MediaCategory, input_genres: list[str]) -> list[int]:
-        tmdb_genres = self._query_genres(category=category)
-        genres_encoder: dict[str, int] = {g["name"].lower(): g["id"] for g in tmdb_genres}
-        try:
-            encoded = [genres_encoder[name.lower()] for name in input_genres]
-        except KeyError as e:
-            raise GenreError(f"Invalid input genre: {e}.")
-        return encoded
-
-    def _decode_genres(self, category: MediaCategory, results: list[dict[str, Any]]) -> list[dict[str, Any]]:
-        tmdb_genres = self._query_genres(category=category)
-        genres_decoder: dict[int, str] = {g["id"]: g["name"] for g in tmdb_genres}
-        try:
-            for item in results:
-                item["genres"] = [genres_decoder[gid] for gid in item["genre_ids"]]
-        except KeyError as e:
-            raise GenreError(f"Unknown genre while processing results: {e}.")
-        return results
-
     def get_genres(self, category: MediaCategory) -> list[str]:
         """Get list of TMDB coded genres from a media category."""
-        return [item["name"] for item in self._query_genres(category=category)]
+        return self.genres_manager.get_genres(category)
 
     def discover(
         self,
@@ -196,35 +279,22 @@ class TMDBClient:
         n_results: int = 20,
     ) -> list[TMDBItem]:
         """Query list of media items from TMDB."""
-        params: dict[str, Any] = {
-            "language": "en-US",
-            "include_adult": False,
-            "include_video": False,
-            "sort_by": sort_by,
-        }
+        params = QueryParams(
+            category=category,
+            genres_manager=self.genres_manager,
+            n_results=n_results,  # ty: ignore[unknown-argument]
+            genres=genres,
+            year_from=year_from,
+            year_to=year_to,
+            min_rating=min_rating,
+            min_votes=min_votes,
+            sort_by=sort_by,
+        )
 
-        if genres is not None:
-            params["with_genres"] = "|".join(
-                [str(g) for g in self._encode_genres(category=category, input_genres=genres)]
-            )
-            logger.debug(f"genres param {params['with_genres']}")
-
-        time_field = TIME_FIELD_MOVIE if category == MediaCategory.movie else TIME_FIELD_TV
-
-        if year_from is not None:
-            params[f"{time_field}.gte"] = f"{year_from}-01-01"
-
-        if year_to is not None:
-            params[f"{time_field}.lte"] = f"{year_to}-12-31"
-
-        if min_rating is not None:
-            params["vote_average.gte"] = min_rating
-
-        if min_votes is not None:
-            params["vote_count.gte"] = min_votes
-
-        data = self._aggregate_results(url=f"/discover/{category.value}", params=params, n_results=n_results)
-        data = self._decode_genres(category=category, results=data)
+        data = self._aggregate_results(
+            url=f"/discover/{category.value}", params=params.parse(), n_results=params.n_results
+        )
+        data = self.genres_manager.decode_genres(category=category, results=data)
 
         item_cls = MovieItem if category == MediaCategory.movie else TVItem
 
